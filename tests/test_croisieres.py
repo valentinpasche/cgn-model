aa5643a9de895aa45599e65aef22612dd7843bdd
Valentin Pasche <228523006+valentinpasche@users.noreply.github.com>
2025-12-04 17:41:59 +0100

    Create test_croisieres.py

---
+++ tests/test_croisieres.py
@@ -0,0 +1,46 @@
+# Test pour valider les croisiere
+# Y compris pour y intéger la "reprise" du delay sur l'étapes suivante
+
+from cgn_model.navigation import Croisiere
+import warnings
+
+
+
+croisieres = Croisiere.from_cgn_croisiere_csv("all")
+
+# ---- Test continuité + __repr__
+for c in croisieres:
+    assert c.check_continuite()
+    print("======"*10)
+    Croisiere.view_croisiere(c)
+
+
+# ---- ALL - Calcul profile vitesse
+
+c_profiles = []
+for c in croisieres:
+    print("======" * 10)
+    print(c.nom)
+    print()
+
+    # On capture les warnings de CETTE croisière uniquement
+    with warnings.catch_warnings(record=True) as wlist:
+        warnings.simplefilter("always")  # forcer l’enregistrement de tous les warnings
+        p, _ = c.speed_profile()
+
+    c_profiles.append(p)
+
+    # Affichage des warnings pour cette croisière
+    if wlist:
+        print("  -- Warnings pour cette croisière --")
+        for w in wlist:
+            # w.message est déjà un objet Warning → str(w.message)
+            print("   ", w.message)
+    else:
+        print("  (aucun warning)")
+
+    print()  # ligne vide pour la lisibilité
+
+
+    
+        
