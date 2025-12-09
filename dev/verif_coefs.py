ee51759aec2cbffe5956bbc1067bbdf3503850f6
Valentin Pasche <228523006+valentinpasche@users.noreply.github.com>
2025-12-09 19:40:55 +0100

    Update verif_coefs.py

--- dev/verif_coefs.py
+++ dev/verif_coefs.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Dec  9 12:51:26 2025
+Test pour valider les coefficients des adaptateurs.
 
-@author: valentin.pasche1
+L'arrondi à 3 chiffres après la virgule change la force de 0.78% W le résulat final...
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
 from numpy.polynomial import polynomial
 
 v_kmh = np.arange(0, 25.45, 0.05)
 
-coefs_base = [0.002, 0.041, 0.529, -0.209]
+coefs_base_full = [0.002038,	0.040937, 0.528592, -0.208722] # Input brut
+coefs_base = [0.002, 0.041, 0.529, -0.209] # Arrondi à 3 chiffres
 coefs_base = list(reversed(coefs_base))
 
 force_base_newton = polynomial.polyval(v_kmh, coefs_base)*1000
@@ -23,7 +23,5 @@ coefs_ms_newton = polynomial.polyfit(v_ms, force_base_newton, 3)
 
 force_ms_newton = polynomial.polyval(v_ms, coefs_ms_newton)
 
-p_ms = force_ms_newton * v_ms
-
 p_kmh = force_base_newton * v_ms
-
+p_ms = force_ms_newton * v_ms
