5e65ae5cee2580ca36de4825d5d10f7e64ee9855
Valentin Pasche <228523006+valentinpasche@users.noreply.github.com>
2025-12-09 20:08:31 +0100

    Update test_vessel_solver_inputs_cal.py
    A faire ce qu'il y a en commentaire lignes 101 et 106/107

--- tests/test_vessel_solver_inputs_cal.py
+++ tests/test_vessel_solver_inputs_cal.py
@@ -1,6 +1,7 @@
 import yaml
 
 from cgn_model.vessel_model import Vessel
+from cgn_model.energy_solver import SolverDAG, prepare_state, run_vector
 
 
 cfg_txt = """
@@ -21,11 +22,18 @@ profiles:
   # 4) Vitesse issue de l’horaire (module navigation)
   - id: "speed"
     kind: "nav_speed"
-    unit: "m/s"
-    source: "cgn_croisieres/all"
+    unit: "m/s"            # unité du profil produit
+    source: "cgn_croisieres/all"     # dataset interne
     select:
-      by: "cruise"
-      cruise_name: "Lavaux - Haut-Lac - Grand-Lac"
+      by: "cruise"         # "cruise" | "course" | "leg"
+      cruise_name: "Lavaux - Haut-Lac - Grand-Lac"     # si by="cruise"
+      course_no: 982                                   # si by="course"
+      leg: { from_port: "Rolle", to_port: "Yvoire" }   # si by="leg"
+    params:                 # MRUA
+      acc: 0.06             # [m/s²]
+      dec: 0.06             # [m/s²]
+      v_croisiere: 7        # [m/s]
+      allow_delay: true
 
 adapters:
     # 1) vitesse -> force (polynôme)
@@ -35,7 +43,7 @@ adapters:
     unit_in: "m/s"
     unit_out: "N"
     params:
-      coeffs: [-208.7, 1902.9, 530.5, 95.1]
+      coeffs: [-209.0, 1904.4, 531.36, 93.312] # coefs vitesse "m/s" to force "N"
     
     # 2) puissance = F * v (multi-entrées)
   - id: "shaft_power_from_Fv"
@@ -87,9 +95,14 @@ converters:
 """
     
 # # === Test de base de la création de la classe Vessel ===
-
 cfg = yaml.safe_load(cfg_txt)
+
 vessel = Vessel.from_yaml(cfg)
-mapping = vessel.build_solver_inputs()
+mapping = vessel.build_solver_inputs() # Signe du profil !!!
 for k, (bus, arr) in mapping.items():
     print(k, "->", bus, "| len:", len(arr), "| first:", round(float(arr[0]),2), "| max:", round(max(arr),2))
+    
+# # === Test combiné avec la création du solveur ===
+prepare_state(vessel.solver, mapping) # Accepter le paramètre Vessel en plus
+run_vector(vessel.solver) # Accepter le paramètre Vessel en plus
+
