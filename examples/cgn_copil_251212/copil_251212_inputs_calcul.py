ebde948cd6a2b2efa92b8c40279c7640174659d5
Valentin Pasche <228523006+valentinpasche@users.noreply.github.com>
2025-12-10 16:39:39 +0100

    Update copil_251212_inputs_calcul.py

--- examples/cgn_copil_251212/copil_251212_inputs_calcul.py
+++ examples/cgn_copil_251212/copil_251212_inputs_calcul.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import yaml
 import pandas as pd
 
@@ -9,38 +8,14 @@ from cgn_model.vessel_model import Vessel
 from cgn_model.energy_solver import run_vector
 
 
-def plot_state(solver, dt: float) -> None:
-    
-    key_0 = list(solver.buses.keys())[0]
-    N = len(solver.buses[key_0].net_w)
-    t = np.arange(0, N*dt, dt)
-    
-    fig, ax = plt.subplots()
-    
-    for conv_id, data in solver.converters.items():
-        p = data.p_out_w
-        ax.plot(t, p, label=conv_id)
-        
-    for input_id, data in solver.inputs.items():
-        ax.plot(t, data.profile, label=input_id)
-    
-    ax.set(xlabel='time (s)', ylabel='Power (W)',
-           title='Profil de puissance, "p_out_w" convertisseurs et inputs')
-    ax.grid()
-    
-    pci_fuel = 9.8 # (kWh / litre)
-    p_chem_net = solver.buses['fuel'].net_w
-    conso_fuel = bus_chem_power_to_stock_fuel(p_chem_net, pci_fuel)
-    
-    ax2 = ax.twinx()
-    ax2.plot(t, conso_fuel, label="cons_fuel", ls='-.', lw=3, color="black")
-    ax2.set_ylabel("Volume de fuel (m^3)")
-    
-    fig.legend()
-    plt.show()
-    
-    return None
-
+def read_yaml(file: str):
+    with open(file) as stream:
+        try:
+            cfg = yaml.safe_load(stream)
+            return cfg
+        except yaml.YAMLError as exc:
+            print(exc)
+            
 def bus_chem_power_to_stock_fuel(p_chem_net, pci_fuel):
     ureg = UnitRegistry()
     pci_fuel = pci_fuel * (ureg.kWh / ureg.liter) # (kWh / litre)
@@ -48,14 +23,30 @@ def bus_chem_power_to_stock_fuel(p_chem_net, pci_fuel):
     tot_fuel = np.cumsum(p_chem_net / pci)
     return tot_fuel
 
-def read_yaml(file):
-    with open(file) as stream:
-        try:
-            cfg = yaml.safe_load(stream)
-            return cfg
-        except yaml.YAMLError as exc:
-            print(exc)
+def export_valeurs(vessel: Vessel, pci_kWh_litre: int) -> pd.DataFrame():
     
+    dt = vessel.dt
+    vitesse = vessel.signals["speed"][0]
+    puissance_arbre = vessel.signals["shaft_power_from_speed"][0]
+    temps = dt*len(vitesse)
+    
+    fuel_net_w = vessel.solver.buses["fuel"].net_w
+    fuel_cumule = bus_chem_power_to_stock_fuel(fuel_net_w, pci_kWh_litre)
+    
+    data = {
+        "temps (s)": temps,
+        "speed (m/s)": vitesse,
+        "power_shaft (W)": puissance_arbre,
+        "fuel_cumul (m^3)": fuel_cumule,
+    }
+    
+    return pd.DataFrame(data)
+
+
+
+
+    
+
 # === Création de la classe Vessel via la config ===
 cfg_file = "config_copil_251212.yaml"
 
@@ -68,33 +59,26 @@ mapping = vessel.apply_inputs_to_solver(verbose=True)
 # === Execution du calcul par le solveur ===
 run_vector(vessel.solver)
 
+# === Export des résultats, DataFrame ===
+pci_kWh_litre = 9.8 # (kWh / litre)
+df = export_valeurs(vessel, pci_kWh_litre)
 
-plot_state(vessel.solver, vessel.dt)
+# # === Sauvegarde des résultats, CSV ===
+# file_csv_output = "values_copil_251212.csv"
+# df.to_csv(file_csv_output, sep=";")
 
-
-dt = vessel.dt
-vitesse = vessel.signals["speed"][0]
-puissance_arbre = vessel.signals["shaft_power_from_speed"][0]
-temps = dt*len(vitesse)
-
-pci_fuel = 9.8 # (kWh / litre)
-fuel_net_w = vessel.solver.buses["fuel"].net_w
-fuel_cumule = bus_chem_power_to_stock_fuel(fuel_net_w, pci_fuel)
-
-data = {
-    "temps (s)": temps,
-    "speed (m/s)": vitesse,
-    "power_shaft (W)": puissance_arbre,
-    "fuel_cumul (m^3)": fuel_cumule,
+# === Graphique du solveur, positions définies ===
+positions_noeux = { # X  ,  Y (centre à 0)
+   'shaft_demand': [-1.00, +1.00],
+   'navops':       [+1.00, +1.00],
+   'shaft':        [-0.50, +0.20],
+   'elec_power':   [+0.50, -0.20],
+   'fuel':         [+1.00, -1.00],
 }
+vessel.solver.draw_dag(pos=positions_noeux)
 
-df = pd.DataFrame(data)
 
-df.to_csv("exemple_copil_251212.csv", sep=";", decimal=".")
-# def export_valeurs(vessel) -> pd.DataFrame:
-    
 
-    
-    
-#     return df
+
+
 
