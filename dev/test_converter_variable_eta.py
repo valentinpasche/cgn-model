e24f808f8dacd5ee7b5cb25bf592191128faa53b
Valentin Pasche <228523006+valentinpasche@users.noreply.github.com>
2025-12-11 16:53:34 +0100

    Create test_converter_variable_eta.py
    Tout fonctionne :)

---
+++ dev/test_converter_variable_eta.py
@@ -0,0 +1,108 @@
+import yaml
+
+from cgn_model.vessel_model import Vessel
+from cgn_model.energy_solver import run_vector
+
+
+cfg_txt = """
+vessel:
+  name: "Vevey"
+  vessel_type: "DE"
+
+simulation:
+  dt: 1.0  # [s]
+
+profiles:
+  - id: "hotel_load"
+    kind: "constant"
+    unit: "W"
+    value: 100e3
+
+  - id: "speed"
+    kind: "nav_speed"
+    unit: "m/s"
+    source: "cgn_croisieres/all"
+    select:
+      by: "cruise"                                # "cruise" | "course" | "leg"
+      cruise_name: "Lavaux - Haut-Lac"                       # si by="cruise"
+      course_no: 982                                         # si by="course"
+      leg: { from_port: "Rolle", to_port: "Yvoire" }         # si by="leg"
+    params:
+      acc: 0.05            # [m/s²]
+      dec: 0.05            # [m/s²]
+      v_croisiere: 7.0     # [m/s]
+      allow_delay: true
+
+adapters:
+  - id: "shaft_power_from_speed"
+    kind: "speed_to_power_poly"
+    source: "speed"
+    unit_in: "m/s"
+    unit_out: "W" 
+    params:
+      coeffs: [0.0, -209.0, 1904.4, 531.36, 93.312]  # coefficients "m/s" to  "W"
+
+  - id: "eta_from_speed"
+    kind: "speed_to_eta_poly"
+    source: "speed"
+    unit_in: "m/s"
+    unit_out: "-"
+    params:
+      coeffs: [0.11138307, 0.03562645, 0.00436722, -0.00056904]  # coefficients "m/s" to η genset
+
+inputs:
+  - id: "shaft_demand"
+    bus: "shaft"
+    source: "shaft_power_from_speed"
+    sign: "consume"
+
+  - id: "navops"
+    bus: "elec_power"
+    source: "hotel_load"
+    sign: "consume"
+
+solver:
+  mode: "inverse"
+
+buses:
+  - id: "shaft"
+    carrier: "Mechanical"
+
+  - id: "elec_power"
+    carrier: "Electrical"
+
+  - id: "fuel"
+    carrier: "Chemical"
+
+converters:
+  - id: "motor"
+    from_bus: "elec_power"
+    to_bus:   "shaft"
+    kind: "constant_eta"
+    params:
+      eta: 0.9   # [-]
+
+  - id: "genset"
+    from_bus: "fuel"
+    to_bus:   "elec_power"
+    kind: "variable_eta"
+    params:
+      eta_default: 0.38   # [-]
+      eta_source: "eta_from_speed"
+
+"""
+    
+# === Test de base ===
+# 0) Initialiser le YAML
+cfg = yaml.safe_load(cfg_txt)
+
+# 1) Construire le Vessel
+vessel = Vessel.from_yaml(cfg)
+
+# 2) Câbler les inputs (prépare les états du solver et fige N)
+vessel.build_solver(verbose=True)
+
+# 3) Lancer la résolution
+run_vector(vessel.solver)
+
+
