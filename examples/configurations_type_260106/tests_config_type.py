2c2301471a3b6cd5099420682187cf369d381385
Valentin Pasche <228523006+valentinpasche@users.noreply.github.com>
2026-06-05 13:11:27 +0200

    Update tests_config_type.py
    Nouveau workflow principal

--- examples/configurations_type_260106/tests_config_type.py
+++ examples/configurations_type_260106/tests_config_type.py
@@ -1,19 +1,19 @@
 # Script demo, configurations types
 
-import yaml
+from pathlib import Path
+
+
+example_dir = Path(__file__).resolve().parent
+
 from cgn_model.vessel_model import Vessel
-from cgn_model.energy_solver import run_vector
 
 
 def process(config_file, draw_dag=False):
 
-    with open(config_file, "r") as f:
-        cfg = yaml.safe_load(f)
+    yaml_text = (example_dir / config_file).read_text(encoding="utf-8")
     
-    vessel = Vessel.from_yaml(cfg)
-    vessel.build_solver(verbose=True)
-    run_vector(vessel.solver)
-    vessel.tally_storages()
+    vessel = Vessel.from_yaml(yaml_text)
+    vessel.run()
     
     if draw_dag:
         vessel.solver.draw_dag()
@@ -26,26 +26,17 @@ def process(config_file, draw_dag=False):
 # --------------------------- Demo ---------------------------
 if __name__ == "__main__":
     
-    pci_mazout = 35.28e9 # J/m3 (9.8 kWh/l)
-    densite_energetique_h2 = 33.3 # kWh/kg
-    j_to_kwh = 3.6e6 # J to kWh (3'600'000 = 1 kWh)
-    
     # ---- 1) Diesel-Electrique
     df_de, units_de, vessel_de = process("config_DE.yaml", draw_dag=False)
-    # df_de["fuel_cum_m3"] = df_de["fuel_tank_e_cum_J"] / pci_mazout
     
     # ---- 2) Vapeur
-    # df_steam, units_steam, _ = process("config_steam.yaml", draw_dag=False)
-    # df_steam["fuel_cum_m3"] = df_steam["fuel_tank_e_cum_J"] / pci_mazout
+    df_steam, units_steam, _ = process("config_steam.yaml", draw_dag=False)
     
     # ---- 3) Full-electrique
-    # df_full_elec, units_full_elec, _ = process("config_full_elec.yaml", draw_dag=False)
-    # df_full_elec["battery_pack_e_cum_kWh"] = df_full_elec["battery_pack_e_cum_J"] / j_to_kwh
+    df_full_elec, units_full_elec, _ = process("config_full_elec.yaml", draw_dag=False)
     
     # ---- 4) H2 - pile combustible
-    # df_h2, units_h2, _ = process("config_H2.yaml", draw_dag=False)
-    # df_h2["h2_tank_e_cum_kWh"] = df_h2["h2_tank_e_cum_J"] / j_to_kwh
-    # df_h2["h2_tank_masse_cum_kg"] = df_h2["h2_tank_e_cum_kWh"] / densite_energetique_h2
+    df_h2, units_h2, _ = process("config_H2.yaml", draw_dag=False)
     
     # ---- 5) Test UI
     df_ui, units_ui, vessel_ui = process("config_from_UI.yaml", draw_dag=False)
